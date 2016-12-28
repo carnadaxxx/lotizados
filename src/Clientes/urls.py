a1bf5808674a6c505b891b6d8a645f0effbad68a
carnadaxxx <w.coronadobarrantes@gmail.com>
2016-12-27 21:35:14 -0500

    agregado de links entre las paginas de detall, creacion y modificacion de cliente

--- src/Clientes/urls.py
+++ src/Clientes/urls.py
@@ -4,5 +4,6 @@ from Clientes import views
 urlpatterns = [
     url(r'^$', views.clientes_list, name='list'),
     url(r'^detail/(?P<id>\d+)/$', views.cliente_detail, name='detail'),
+    url(r'^update/(?P<id>\d+)/$', views.cliente_update, name='update'),
     url(r'^create/$', views.cliente_create, name='create'),
 ]
--- src/Clientes/views.py
+++ src/Clientes/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from .forms import ClienteForm
@@ -26,8 +26,39 @@ def cliente_create(request):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
     context = {
+        "titulo": "Creacion de Cliente",
         "form": form,
     }
-
     return render(request, "clientes/form.html", context)
+
+def cliente_update(request, id=None):
+    queryset = get_object_or_404(Cliente, id=id)
+    if request.method == 'POST':
+        form = ClienteForm(request.POST or None, instance=instance)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save()
+            # return HttpResponseRedirect(instance.get_absolute_url())
+    else:
+        form = ClienteForm()
+    context = {
+        "titulo": "Editar informacion del Cliente",
+        "queryset": queryset,
+        "form": form,
+    }
+    return render(request, "clientes/form.html", context)
+
+    # queryset = get_object_or_404(Cliente, id=id)
+    # form = ClienteForm(request.POST or None, instance=instance)
+    # if form.is_valid():
+    #     instance = form.save(commit=False)
+    #     instance.save()
+    #     return HttpResponseRedirect(instance.get_absolute_url())
+    # context = {
+    #     "titulo": "Editar del Cliente",
+    #     "queryset": queryset,
+    #     "form": form,
+    # }
+    # return render(request, "clientes/form.html", context)
